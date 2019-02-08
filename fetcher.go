c964488d6d2c9b0df5812d85aae80624356c5dcc
Ibrahim Jarif <jarifibrahim@gmail.com>
2019-02-08 21:35:35 +0530

    Add fetcher and fetcher_test.go

---
+++ fetcher.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/html"
+)
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) ([]string, error)
+}
+
+type Client interface {
+	Get(string) (*http.Response, error)
+}
+
+// SimpleFetcher implements Fetcher
+type SimpleFetcher struct {
+	client  Client
+	baseURL string
+}
+
+// NewSimpleFetcher creates a new fetcher with the given base URL. It also
+// creates a new http.Client with 3 seconds timeout
+func NewSimpleFetcher(url string) *SimpleFetcher {
+	simpleClient := http.DefaultClient
+	simpleClient.Timeout = 3 * time.Second
+	return &SimpleFetcher{baseURL: url, client: simpleClient}
+
+}
+
+func (f SimpleFetcher) Fetch(url string) ([]string, error) {
+	contextLogger := log.WithField("url", url)
+
+	resp, err := f.client.Get(url)
+	if err != nil {
+		contextLogger.Errorf("Failed to fetch URL: %s", err)
+		return nil, fmt.Errorf("Failed to fetch URL: %s", err)
+	}
+
+	defer resp.Body.Close()
+	return ConvertResponseToURLList(f.baseURL, resp.Body), nil
+}
+
+func ConvertResponseToURLList(baseURL string, body io.Reader) []string {
+	var urlList []string
+	// URLset is used to ensure urlList is always unique
+	URLset := make(map[string]struct{})
+	tokenizer := html.NewTokenizer(body)
+	for {
+		tt := tokenizer.Next()
+		switch tt {
+		case html.ErrorToken:
+			return urlList
+		case html.StartTagToken:
+			token := tokenizer.Token()
+			// Token isn't a <a> tag. Skip token and continue the loop
+			if token.DataAtom.String() != "a" {
+				continue
+			}
+			href := findHrefValue(token)
+			if href == nil {
+				continue
+			}
+			builtURL, err := buildURL(baseURL, *href)
+			if err != nil {
+				log.Errorf("Failed to build URL: %s", err)
+				continue
+			}
+			// If we've already added this URL to urlList, don't add it again.
+			if _, ok := URLset[builtURL]; ok || builtURL == baseURL {
+				continue
+			}
+
+			// add URL to URLset
+			URLset[builtURL] = struct{}{}
+
+			if err != nil {
+				// error occurred while trying to build the URL. Log the error
+				// and continue.
+				log.Errorf("failed to build URL: %s", err)
+			}
+			urlList = append(urlList, builtURL)
+		}
+	}
+}
+
+func findHrefValue(t html.Token) *string {
+	for _, attr := range t.Attr {
+		if attr.Key == "href" || attr.Key == "HREF" {
+			return &attr.Val
+		}
+	}
+	return nil
+}
+
+func buildURL(baseURL string, href string) (string, error) {
+
+	// Invalid href value. Return error
+	if href == "" ||
+		len(href) == 1 ||
+		strings.HasPrefix(href, "#") {
+		return "", fmt.Errorf("Invalid URL: %s", href)
+	}
+
+	u, err := url.Parse(href)
+	if err != nil {
+		return "", err
+	}
+	// Remove all query params
+	u.RawQuery = ""
+	// Remove fragment, if any
+	u.Fragment = ""
+	base, err := url.Parse(baseURL)
+	if err != nil {
+		return "", err
+	}
+	return base.ResolveReference(u).String(), nil
+}
---
+++ fetcher_test.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	"golang.org/x/net/html/atom"
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/html"
+)
+
+type fakeClient struct {
+	responseCache map[string]string
+}
+
+func (fc fakeClient) Get(url string) (*http.Response, error) {
+	if res, ok := fc.responseCache[url]; ok {
+		return &http.Response{
+			Body: ioutil.NopCloser(strings.NewReader(res)),
+		}, nil
+	}
+	return nil, fmt.Errorf("not found: %s", url)
+}
+func TestSimpleFetcher(t *testing.T) {
+	fakeClient := fakeClient{
+		responseCache: map[string]string{
+			"http://localhost:8000/foobar": `
+			<html>
+			<body>
+				<a href="/hello">Hello, World!</a>
+				<p>Lorem Ipsum</p>
+			    <div>
+				    <a href="/bye">Bye, World!</a>
+				</div>
+				<A HREF="/BYE">BYE, WORLD!</a>
+            	<a href="#">$$$</a>
+			</body>
+		    </html>`,
+		},
+	}
+	// Ensure fakeClient conforms to the Client interface
+	var _ Client = fakeClient
+
+	testFetcher := NewSimpleFetcher("http://localhost:8000/foobar")
+	testFetcher.client = fakeClient
+	t.Run("success", func(t *testing.T) {
+		result, err := testFetcher.Fetch(testFetcher.baseURL)
+		assert.Nil(t, err)
+		assert.Equal(t, result, []string{"http://localhost:8000/hello", "http://localhost:8000/bye", "http://localhost:8000/BYE"})
+	})
+	t.Run("client error", func(t *testing.T) {
+		result, err := testFetcher.Fetch("my/random/url")
+		assert.Nil(t, result)
+		assert.Error(t, err)
+	})
+}
+func TestBuildURL(t *testing.T) {
+	testData := []struct {
+		name        string
+		baseURL     string
+		childURL    string
+		result      string
+		expectError bool
+	}{
+		{
+			"relative URL",
+			"http://foo.com",
+			"/bar",
+			"http://foo.com/bar",
+			false,
+		}, {
+			"absolute URL",
+			"http://foo.com",
+			"http://foo.com/bar",
+			"http://foo.com/bar",
+			false,
+		}, {
+			"invalid base URL",
+			"foo.....com",
+			"/bar",
+			"/bar",
+			false,
+		},
+	}
+	for _, tt := range testData {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			builtURL, err := buildURL(tt.baseURL, tt.childURL)
+			if tt.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.Nil(t, err)
+			}
+			assert.Equal(t, tt.result, builtURL)
+		})
+	}
+}
+func TestFindHref(t *testing.T) {
+	Validtoken := html.Token{
+		Type:     html.StartTagToken,
+		DataAtom: atom.Lookup([]byte("a")),
+		Data:     "",
+	}
+	t.Run("non-empty href", func(t *testing.T) {
+		expectedHref := "http://foo.com"
+		Validtoken.Attr = []html.Attribute{html.Attribute{Key: "href", Val: expectedHref}}
+
+		assert.Equal(t, expectedHref, *findHrefValue(Validtoken))
+	})
+	t.Run("non-empty HREF", func(t *testing.T) {
+		expectedHref := "http://foo.com"
+		Validtoken.Attr = []html.Attribute{html.Attribute{Key: "HREF", Val: expectedHref}}
+
+		assert.Equal(t, expectedHref, *findHrefValue(Validtoken))
+	})
+	t.Run("empty href", func(t *testing.T) {
+		expectedHref := ""
+		Validtoken.Attr = []html.Attribute{html.Attribute{Key: "HREF", Val: expectedHref}}
+
+		assert.Equal(t, expectedHref, *findHrefValue(Validtoken))
+	})
+	t.Run("missing href", func(t *testing.T) {
+		assert.Equal(t, "", *findHrefValue(Validtoken))
+	})
+}
+
+func TestConvertResponseToURLList(t *testing.T) {
+	mockFetcher := SimpleFetcher{baseURL: "http://site.com"}
+	testData := []struct {
+		testName        string
+		response        string
+		expectedURLList []string
+	}{
+		{"empty string", "", nil},
+		{"no <a> tags", "<html></html>", nil},
+		{"one <a> tag", "<a href='/bar/foo'></a>", []string{"http://site.com/bar/foo"}},
+		{"multiple <a> tags", "<a href='/foo'></a><A HREF='/bar'></A>", []string{"http://site.com/foo", "http://site.com/bar"}},
+		{"<a> tag without href", "<a class='btn'></a>", nil},
+		{"<a> tag with absolute URL", "<a href='http://foo.com'></a><a href='/bar'></a>", []string{"http://foo.com", "http://site.com/bar"}},
+		{"<a> tag with # href", "<a href='#content'></a>", nil},
+		{"duplicate <a> tags", "<a href='/foo'></a><a href='/foo'></a>", []string{"http://site.com/foo"}},
+	}
+
+	for _, tt := range testData {
+		tt := tt
+		t.Run(tt.testName, func(t *testing.T) {
+			body := strings.NewReader(tt.response)
+			actualURLList := ConvertResponseToURLList(mockFetcher.baseURL, body)
+			assert.Equal(t, tt.expectedURLList, actualURLList)
+		})
+	}
+}
