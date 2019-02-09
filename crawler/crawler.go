df7733234c89d61d4615aa8b023763d5d5902e99
Ibrahim Jarif <jarifibrahim@gmail.com>
2019-02-09 18:49:25 +0530

    Remove crawledURLs variable. Store all the urls in seenURLs struct

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,9 +1,11 @@
 package crawler
 
 import (
+	"fmt"
 	"io"
 	"net/url"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/alecthomas/template"
@@ -11,6 +13,31 @@ import (
 	"github.com/jarifibrahim/webcrawler/tree"
 	log "github.com/sirupsen/logrus"
 )
+
+type seenURLs struct {
+	urlMap map[string]struct{}
+	urls   []string
+
+	sync.Mutex
+}
+
+func newSeenMap() *seenURLs {
+	return &seenURLs{
+		urlMap: make(map[string]struct{}),
+	}
+}
+func (s *seenURLs) Add(url string) bool {
+	s.Lock()
+	if _, ok := s.urlMap[url]; ok {
+		// URL already present. Return false indicating the new url was already present
+		s.Unlock()
+		return false
+	}
+	s.urlMap[url] = struct{}{}
+	s.urls = append(s.urls, url)
+	s.Unlock()
+	return true
+}
 
 /*
 crawl fetches the list of URLs using the fetcher and crawls the new list of
@@ -24,26 +51,20 @@ Params:
 	seenURL - Set of URLs already crawled. This ensures we do not crawl URLs which are
 			  already crawled.
 */
-func crawl(baseURL string, depth int, fetcher fetchers.Fetcher, urlNode *tree.URLNode, seenURL map[string]bool) *[]string {
+func crawl(baseURL string, depth int, fetcher fetchers.Fetcher, urlNode *tree.URLNode, seenURL *seenURLs) {
 	contextLogger := log.WithFields(log.Fields{
 		"base_url": baseURL,
 		"depth":    depth,
 	})
-	// URL is already crawled. Skip crawling it.
-	if seenURL[baseURL] {
+	// seenURL.Add returns false means the URL was already seen.
+	if !seenURL.Add(baseURL) {
 		contextLogger.Info("URL already crawled. Skipping")
-		return nil
+		return
 	}
-
-	// Mark current URL as crawled
-	seenURL[baseURL] = true
-
-	// Create crawledURLs to store new list of crawled URLs
-	crawledURLs := &[]string{baseURL}
 
 	if depth < 1 {
 		contextLogger.Info("Max depth reached. Skipping")
-		return crawledURLs
+		return
 	}
 
 	contextLogger.Infof("Started crawling page")
@@ -54,7 +75,7 @@ func crawl(baseURL string, depth int, fetcher fetchers.Fetcher, urlNode *tree.UR
 
 	if err != nil {
 		contextLogger.Infof("failed to fetch URL")
-		return crawledURLs
+		return
 	}
 
 	for _, url := range urlList {
@@ -62,16 +83,14 @@ func crawl(baseURL string, depth int, fetcher fetchers.Fetcher, urlNode *tree.UR
 		childNode := urlNode.AddChild(url)
 
 		if !isPartOfDomain(baseURL, url) {
+			// even if we're not crawling the URLs, mark it as seen
+			seenURL.Add(url)
 			contextLogger.WithField("child_url", url).Info("Child URL not part of the domain. Skipping.")
 			continue
 		}
-		newlyCrawledURLs := crawl(url, depth-1, fetcher, childNode, seenURL)
-		if newlyCrawledURLs != nil {
-			// Add newly crawled URLs to the list of crawled URLs
-			*crawledURLs = append(*crawledURLs, *newlyCrawledURLs...)
-		}
+		crawl(url, depth-1, fetcher, childNode, seenURL)
 	}
-	return crawledURLs
+	return
 }
 
 func isPartOfDomain(baseURL, urlToCheck string) bool {
@@ -94,10 +113,9 @@ func StartCrawling(maxDepth int, baseURL string, file *os.File) {
 	start := time.Now()
 
 	root := tree.NewNode(baseURL)
-	visited := make(map[string]bool)
-
-	crawl(baseURL, maxDepth, fetchers.NewSimpleFetcher(baseURL), root, visited)
-
+	seenURLs := newSeenMap()
+	crawl(baseURL, maxDepth, fetchers.NewSimpleFetcher(baseURL), root, seenURLs)
+	fmt.Println(seenURLs.urls)
 	log.Info("Total time taken:", time.Since(start))
 
 	root.WriteTreeToFile(file)
--- crawler/crawler_test.go
+++ crawler/crawler_test.go
@@ -70,15 +70,15 @@ func TestIsPartOfDomain(t *testing.T) {
 func TestCrawlDepth0(t *testing.T) {
 	expectedURLList := []string{"https://g.org/"}
 	t.Run("without Tree", func(t *testing.T) {
-		visited := make(map[string]bool)
-		crawledURLs := crawl("https://g.org/", 0, ffetcher, nil, visited)
-		assert.Equal(t, expectedURLList, *crawledURLs)
+		visited := newSeenMap()
+		crawl("https://g.org/", 0, ffetcher, nil, visited)
+		assert.Equal(t, expectedURLList, visited.urls)
 	})
 	t.Run("with Tree", func(t *testing.T) {
-		visited := make(map[string]bool)
+		visited := newSeenMap()
 		rootNode := tree.NewNode("https://g.org/")
-		crawledURLs := crawl("https://g.org/", 0, ffetcher, rootNode, visited)
-		assert.Equal(t, expectedURLList, *crawledURLs)
+		crawl("https://g.org/", 0, ffetcher, rootNode, visited)
+		assert.Equal(t, expectedURLList, visited.urls)
 		// A tree with depth 0 is only the root node
 		expectedTree := tree.NewNode("https://g.org/")
 		assert.Equal(t, expectedTree, rootNode)
@@ -87,16 +87,16 @@ func TestCrawlDepth0(t *testing.T) {
 func TestCrawlDepth1(t *testing.T) {
 	expectedURLList := []string{"https://g.org/", "https://g.org/pkg/", "https://g.org/cmd/"}
 	t.Run("without tree", func(t *testing.T) {
-		visited := make(map[string]bool)
-		crawledURLs := crawl("https://g.org/", 1, ffetcher, nil, visited)
-		assert.Equal(t, expectedURLList, *crawledURLs)
+		visited := newSeenMap()
+		crawl("https://g.org/", 1, ffetcher, nil, visited)
+		assert.Equal(t, expectedURLList, visited.urls)
 
 	})
 	t.Run("with tree", func(t *testing.T) {
-		visited := make(map[string]bool)
+		visited := newSeenMap()
 		rootNode := tree.NewNode("https://g.org/")
-		crawledURLs := crawl("https://g.org/", 1, ffetcher, rootNode, visited)
-		assert.Equal(t, expectedURLList, *crawledURLs)
+		crawl("https://g.org/", 1, ffetcher, rootNode, visited)
+		assert.Equal(t, expectedURLList, visited.urls)
 
 		expectedTree := tree.NewNode("https://g.org/")
 		for _, url := range expectedURLList[1:] {
@@ -106,9 +106,9 @@ func TestCrawlDepth1(t *testing.T) {
 	})
 	t.Run("non existent URL", func(t *testing.T) {
 		t.Run("without tree", func(t *testing.T) {
-			visited := make(map[string]bool)
-			crawledURLs := crawl("https://foo.org/", 1, ffetcher, nil, visited)
-			assert.Equal(t, []string{"https://foo.org/"}, *crawledURLs)
+			visited := newSeenMap()
+			crawl("https://foo.org/", 1, ffetcher, nil, visited)
+			assert.Equal(t, []string{"https://foo.org/"}, visited.urls)
 		})
 	})
 }
@@ -117,15 +117,15 @@ func TestCrawlDepth2(t *testing.T) {
 		"https://g.org/", "https://g.org/pkg/", "https://g.org/cmd/",
 		"https://g.org/pkg/fmt/", "https://g.org/pkg/os/"}
 	t.Run("without tree", func(t *testing.T) {
-		visited := make(map[string]bool)
-		crawledURLs := crawl("https://g.org/", 2, ffetcher, nil, visited)
-		assert.Equal(t, expectedURLs, *crawledURLs)
+		visited := newSeenMap()
+		crawl("https://g.org/", 2, ffetcher, nil, visited)
+		assert.Equal(t, expectedURLs, visited.urls)
 	})
 	t.Run("with tree", func(t *testing.T) {
-		visited := make(map[string]bool)
+		visited := newSeenMap()
 		rootNode := tree.NewNode("https://g.org/")
-		crawledURLs := crawl("https://g.org/", 2, ffetcher, rootNode, visited)
-		assert.Equal(t, expectedURLs, *crawledURLs)
+		crawl("https://g.org/", 2, ffetcher, rootNode, visited)
+		assert.Equal(t, expectedURLs, visited.urls)
 
 		expectedTree := tree.NewNode("https://g.org/")
 		child := expectedTree.AddChild("https://g.org/pkg/")
@@ -144,15 +144,15 @@ func TestCrawlDepth3(t *testing.T) {
 		"https://g.org/pkg/fmt/", "https://g.org/pkg/os/"}
 
 	t.Run("without tree", func(t *testing.T) {
-		visited := make(map[string]bool)
-		crawledURLs := crawl("https://g.org/", 3, ffetcher, nil, visited)
-		assert.Equal(t, depth3ExpectedURLs, *crawledURLs)
+		visited := newSeenMap()
+		crawl("https://g.org/", 3, ffetcher, nil, visited)
+		assert.Equal(t, depth3ExpectedURLs, visited.urls)
 	})
 	t.Run("with tree", func(t *testing.T) {
-		visited := make(map[string]bool)
+		visited := newSeenMap()
 		rootNode := tree.NewNode("https://g.org/")
-		crawledURLs := crawl("https://g.org/", 3, ffetcher, rootNode, visited)
-		assert.Equal(t, depth3ExpectedURLs, *crawledURLs)
+		crawl("https://g.org/", 3, ffetcher, rootNode, visited)
+		assert.Equal(t, depth3ExpectedURLs, visited.urls)
 
 		expectedTree := tree.NewNode("https://g.org/")
 
@@ -179,21 +179,51 @@ func TestCrawlDepth3(t *testing.T) {
 		// The output of crawledURLs depth 3 onwards should be same since there aren't
 		// anymore URLs below dept 3
 		t.Run("depth 4", func(t *testing.T) {
-			visited := make(map[string]bool)
+			visited := newSeenMap()
 			rootNode := tree.NewNode("https://g.org/")
-			crawledURLs := crawl("https://g.org/", 4, ffetcher, rootNode, visited)
-			assert.Equal(t, depth3ExpectedURLs, *crawledURLs)
+			crawl("https://g.org/", 4, ffetcher, rootNode, visited)
+			assert.Equal(t, depth3ExpectedURLs, visited.urls)
 			assert.Equal(t, expectedTree, rootNode)
 		})
 		t.Run("depth 5", func(t *testing.T) {
-			visited := make(map[string]bool)
+			visited := newSeenMap()
 			rootNode := tree.NewNode("https://g.org/")
-			crawledURLs := crawl("https://g.org/", 5, ffetcher, rootNode, visited)
-			assert.Equal(t, depth3ExpectedURLs, *crawledURLs)
+			crawl("https://g.org/", 5, ffetcher, rootNode, visited)
+			assert.Equal(t, depth3ExpectedURLs, visited.urls)
 			assert.Equal(t, expectedTree, rootNode)
 		})
 	})
 
+}
+
+func TestActualWebsite(t *testing.T) {
+	expectedURLs := []string{"http://jarifibrahim.github.io",
+		"http://jarifibrahim.github.io/blog/",
+		"http://jarifibrahim.github.io/about/",
+		"https://github.com/jarifibrahim",
+		"https://facebook.com/jarifibrahim2",
+		"https://twitter.com/_iamibrahim",
+		"mailto:jarifibrahim@gmail.com",
+		"https://linkedin.com/in/jarifibrahim",
+		"http://jarifibrahim.github.io/blog/test-cleanup-with-gorm-hooks/",
+		"https://github.com/jinzhu/gorm",
+		"http://gorm.io/docs/hooks.html",
+		"http://jarifibrahim.github.io/tags/golang/",
+		"http://jarifibrahim.github.io/tags/go/",
+		"http://jarifibrahim.github.io/tags/gorm/",
+		"http://jarifibrahim.github.io/tags/software-testing/",
+		"http://jarifibrahim.github.io/blog/protractor-and-page-synchronization/",
+		"http://jarifibrahim.github.io/blog/golden-files-why-you-should-use-them/",
+		"https://github.com/fabric8-services/fabric8-wit/blob/master/controller/test-files/label/update/ok.label.golden.json",
+		"https://github.com/fabric8-services/fabric8-wit/blob/master/controller/golden_files_test.go",
+		"https://github.com/kwk",
+		"https://github.com/fabric8-services/fabric8-wit/tree/master/controller/test-files",
+		"http://vincent.demeester.fr/posts/2017-04-22-golang-testing-golden-file/",
+		"https://medium.com/@povilasve/go-advanced-tips-tricks-a872503ac859", "http://jarifibrahim.github.io/tags/golden-files/",
+	}
+	seenURLs := newSeenMap()
+	crawl("http://jarifibrahim.github.io", 3, fetchers.NewSimpleFetcher("http://jarifibrahim.github.io"), nil, seenURLs)
+	assert.Equal(t, expectedURLs, seenURLs.urls)
 }
 
 // Inspired from https://tour.golang.org/concurrency/10
